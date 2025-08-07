b19812b51b89abe896d5eccca79981302f8ff3d2
TJK98 <wtj1998@naver.com>
2025-08-07 12:10:37 +0900

    로그인 및 회원가입 페이지 템플릿 추가: Thymeleaf와 Bootstrap을 활용한 로그인 및 회원가입 HTML/CSS 작성, 관련 JS 모듈 구현 및 API 유틸리티 추가.

---
+++ src/main/java/com/spring/toyproject/routes/PageController.java
@@ -0,0 +1,32 @@
+package com.spring.toyproject.routes;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 페이지 전환 렌더링용 컨트롤러
+ * Tyymeleaf나 JSP 같은 뷰템플릿 페이지를 랜더링
+ */
+@Controller
+@Slf4j
+public class PageController {
+
+    // 홈으로 이동
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    // 로그인 페이지로 이동
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    // 회원가입 페이지로 이동
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+}
