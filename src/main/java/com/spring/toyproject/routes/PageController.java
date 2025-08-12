06c7c3862e7aad53aa7e45bf8ff0810b357cf514
TJK98 <wtj1998@naver.com>
2025-08-12 15:37:45 +0900

    대시보드 및 여행 목록 페이지 구현: 대시보드와 여행 목록의 Thymeleaf 템플릿 추가, 페이지 전환 관련 컨트롤러 수정, TripRepository 생성 및 UserRepository 패키지 구조 변경.

--- src/main/java/com/spring/toyproject/routes/PageController.java
+++ src/main/java/com/spring/toyproject/routes/PageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * 페이지 전환 렌더링용 컨트롤러
- * Tyymeleaf나 JSP 같은 뷰템플릿 페이지를 랜더링
+ * Thymeleaf나 JSP같은 뷰템플릿 페이지를 렌더링
  */
 @Controller
 @Slf4j
@@ -17,16 +17,26 @@ public class PageController {
     public String home() {
         return "index";
     }
-
-    // 로그인 페이지로 이동
+    // 로그인페이지으로 이동
     @GetMapping("/login")
     public String login() {
         return "login";
     }
-
     // 회원가입 페이지로 이동
     @GetMapping("/signup")
     public String signup() {
         return "signup";
+    }
+
+    // 대시보드 페이지로 이동
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard";
+    }
+
+    // 여행목록 페이지로 이동
+    @GetMapping("/trips")
+    public String trips() {
+        return "trip-list";
     }
 }
